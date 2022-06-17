27e568ce3bbf649b5f74cc2222548c97c6649cb1
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-06-17 13:43:50 +0100

    ecommerce

---
+++ py.py
@@ -0,0 +1,17 @@
+name = 'omo'
+print(f"my name is {name}")
+print("my name is " + name)
+print('my name is {}'.format(name))
+print(round(1.3555))
+b = "False"
+a = 'yes {}'
+print(a.format(b))
+months = " jan \t*mar"
+print(months)
+#print("how old are you", end=' ')
+#age = input()
+#print("are you fem", end=" ")
+#fem = input()
+#print(f"you are {fem} of age{age}")
+way = input("the way?")
+print(f"{way}")
